282c87474b6b00a52ab6e25a35445d3d5adb1072
corinnpope <corinn.pope@gmail.com>
2017-09-27 21:41:00 -0500

    added basic model

--- stractic/models.py
+++ stractic/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Resource(models.Model):
+	resource_name = models.CharField(max_length=200)
+	resource_url = models.CharField(max_length=300)
+	resource_description = models.CharField(max_length=2000)
+
+	def __str__(self):
+		return self.resource_name
+
+class Favorite(models.Model):
+	resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+	favorited = models.BooleanField()
+	comments = models.CharField(max_length=280)
+	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.resource
