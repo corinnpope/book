4ce87e66dfd93319269a821fa0149d2c07bbb7e4
corinnpope <corinn.pope@gmail.com>
2017-10-06 14:23:49 -0500

    added base.html to a file

--- mysaas/urls.py
+++ mysaas/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from stractic.views import index
 
 urlpatterns = [
+	url(r'^$', index),
     url(r'^admin/', admin.site.urls),
     url(r'^stractic/', include('stractic.urls')),
 
